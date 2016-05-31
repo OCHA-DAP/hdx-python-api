da59566ca7ae7c2c6ae55ab0176e7f36cf0865bc
Michael Rans <rans@email.com>
2016-05-31 16:43:42 +0200

    Incldue config folder

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
-setup(
-    name = "hdx-python-api",
-    version = "0.1",
-    packages = find_packages(),
 
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
+setup(
+    name='hdx-python-api',
+    version='0.1',
+    packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']) + ['config'],
+    url='http://data.humdata.org/',
+    license='PSF',
+    author='Michael Rans',
+    author_email='rans@email.com',
+    description='HDX Python Library',
+
     install_requires = ['colorlog>=2.6.3',
                         'ndg-httpsclient>=0.4.0',
                         'pyasn1>=0.1.9',
@@ -14,20 +18,10 @@ setup(
                         'requests>=2.9.1',
                         'scraperwiki>=0.5.1'
                         ],
-
     package_data = {
-        # If any package contains *.txt or *.rst files, include them:
+        # If any package contains *.txt or *.md files, include them:
         '': ['*.txt', '*.md'],
         # And include any *.yml files found in the 'config' package, too:
         'config': ['*.yml'],
     },
-
-    # metadata for upload to PyPI
-    author = "Michael Rans",
-    author_email = "rans@email.com",
-    description = "HDX Python Library",
-    license = "PSF",
-    keywords = "HDX Python API",
-    url = "http://data.humdata.org/"   # project home page
-
 )
