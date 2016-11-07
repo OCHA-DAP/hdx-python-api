e0861d2c9c61c956e6b675cbdd512a83edcafa20
Michael Rans <rans@email.com>
2016-11-07 16:41:27 +0100

    Remove dependency on typing  from setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,42 @@
-from os.path import join
+import inspect
+import sys
+from os.path import join, abspath, realpath, dirname
 
 from setuptools import setup, find_packages
 
-from hdx.utilities.path import script_dir_plus_file
+
+# Sadly we cannot use the utilities here because of the typing module which isn't in Python < 3.5
+def script_dir(pyobject, follow_symlinks=True):
+    """Get current script's directory
+
+    Args:
+        pyobject (Any): Any Python object in the script
+        follow_symlinks (Optional[bool]): Follow symlinks or not. Defaults to True.
+
+    Returns:
+        str: Current script's directory
+    """
+    if getattr(sys, 'frozen', False):  # py2exe, PyInstaller, cx_Freeze
+        path = abspath(sys.executable)
+    else:
+        path = inspect.getabsfile(pyobject)
+    if follow_symlinks:
+        path = realpath(path)
+    return dirname(path)
+
+
+def script_dir_plus_file(filename, pyobject, follow_symlinks=True):
+    """Get current script's directory and then append a filename
+
+    Args:
+        filename (str): Filename to append to directory path
+        pyobject (Any): Any Python object in the script
+        follow_symlinks (Optional[bool]): Follow symlinks or not. Defaults to True.
+
+    Returns:
+        str: Current script's directory and with filename appended
+    """
+    return join(script_dir(pyobject, follow_symlinks), filename)
 
 
 def get_version():
