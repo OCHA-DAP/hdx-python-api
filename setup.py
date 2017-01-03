016c4023cab99629d0386f1b1ec310bfc39e7930
Michael Rans <rans@email.com>
2017-01-03 15:32:47 +0100

    Version 0.70 - try to pull in correct dependency

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
 
     install_requires=requirements,
     dependency_links=[
-        'git+git://github.com/ckan/ckanapi.git@master#egg=ckanapi'
+        'git+git://github.com/ckan/ckanapi/tarball/master#egg=ckanapi'
     ],
     package_data={
         # Include version.txt and if any package contains *.yml files, include them:
