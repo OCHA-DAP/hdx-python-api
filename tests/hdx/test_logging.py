e80e0c0340ee89c17fe0e617736b1e01062488c3
Brandon Liu <bdon@bdon.org>
2017-03-21 13:35:32 -0400

    rename logging and scraperwiki - module name clash on python 2.7 [#1]
    * rename logging to hdx_logging and scraperwiki to hdx_scraperwiki

--- tests/hdx/test_logging.py
+++ tests/hdx/test_logging.py
@@ -5,7 +5,7 @@ from os.path import join
 import pytest
 from logging_tree import format
 
-from hdx.logging import setup_logging, LoggingError
+from hdx.hdx_logging import setup_logging, LoggingError
 
 try:
     FILENOTFOUND_EXCTYPE = FileNotFoundError
