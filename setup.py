2fa9027ac51f1f17195b5e91e661da0d79199454
Mike <rans@email.com>
2018-01-22 09:20:16 +0100

    Update HDX Python country dependency

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ def get_readme():
 
 
 requirements = ['ckanapi>=4.1',
-                'hdx-python-country>=1.1.4',
+                'hdx-python-country>=1.1.5',
                 'ndg-httpsclient',
                 'pyasn1',
                 'pyOpenSSL',
