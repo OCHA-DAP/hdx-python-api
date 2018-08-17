7e50e1704be41a13df01d2ed700de75c7f094f55
Mike <rans@email.com>
2018-08-17 16:05:48 +0200

    Allow lookup in user agent YAML (so file can contain many agents)

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ def get_readme():
 
 
 requirements = ['ckanapi>=4.1',
-                'hdx-python-country>=1.3.0',
+                'hdx-python-country>=1.3.1',
                 'ndg-httpsclient',
                 'pyasn1',
                 'pyOpenSSL',
