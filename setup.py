c9f2131babb3cb9c558e70ff2e94043dfeb4ea6d
Mike <rans@email.com>
2018-11-29 11:44:41 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ def get_readme():
 
 
 requirements = ['ckanapi>=4.1',
-                'hdx-python-country>=1.5.0',
+                'hdx-python-country>=1.5.1',
                 'ndg-httpsclient',
                 'pyasn1',
                 'pyOpenSSL',
