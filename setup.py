4d1f8655beda5d4195a4405cb29bace03fe6d0db
Mike <rans@email.com>
2020-10-20 16:25:27 +0200

    Update requirements.txt

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from hdx.utilities.loader import load_file_to_str
 from setuptools import setup, find_packages
 
 requirements = ['ckanapi>=4.3',
-                'hdx-python-country>=2.6.1',
+                'hdx-python-country>=2.6.2',
                 'ndg-httpsclient',
                 'pyasn1',
                 'pyOpenSSL',
