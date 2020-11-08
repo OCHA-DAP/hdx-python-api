68730b40f49797afb19e45190747709734cfeaf1
Mike <rans@email.com>
2020-11-09 06:58:24 +1300

    Pin CKANAPI to 4.3 until a fix is forthcoming

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from hdx.utilities import CleanCommand, PackageCommand, PublishCommand
 from hdx.utilities.loader import load_file_to_str
 from setuptools import setup, find_packages
 
-requirements = ['ckanapi>=4.4',
+requirements = ['ckanapi == 4.3',
                 'hdx-python-country>=2.6.4',
                 'ndg-httpsclient',
                 'pyasn1',
