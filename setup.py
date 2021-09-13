8d157eab3e9fe530a1cd995bcba5af6a5f207be8
Mike <rans@un.org>
2021-09-14 11:46:42 +1200

    Move most of setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -2,52 +2,15 @@ from os.path import join
 
 from hdx.utilities import CleanCommand, PackageCommand, PublishCommand
 from hdx.utilities.loader import load_file_to_str
-from setuptools import find_namespace_packages, setup
-
-requirements = [
-    "ckanapi >= 4.6",
-    "hdx-python-country>=3.0.2",
-    "ndg-httpsclient",
-    "pyasn1",
-    "pyOpenSSL",
-    "quantulum3",
-]
-
-classifiers = [
-    "Development Status :: 5 - Production/Stable",
-    "Intended Audience :: Developers",
-    "Natural Language :: English",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-]
+from setuptools import setup
 
 PublishCommand.version = load_file_to_str(join("src", "hdx", "version.txt"), strip=True)
 
 setup(
-    name="hdx-python-api",
-    description="HDX Python Library",
-    license="MIT",
-    url="https://github.com/OCHA-DAP/hdx-python-api",
     version=PublishCommand.version,
-    author="Michael Rans",
-    author_email="rans@email.com",
-    keywords=["HDX", "API", "library"],
-    long_description=load_file_to_str("README.md"),
-    long_description_content_type="text/markdown",
-    packages=find_namespace_packages(where="src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
-    zip_safe=True,
-    classifiers=classifiers,
-    install_requires=requirements,
     cmdclass={
         "clean": CleanCommand,
         "package": PackageCommand,
-        "publish": PublishCommand,
-    },
+        "publish": PublishCommand
+    }
 )
